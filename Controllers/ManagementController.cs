3950f6fb943e106fcea9dc8a89db0827e1298e3e
Ratnesh Ravi <ratneshravi112@gmail.com>
2022-05-06 15:16:51 +0800

    Add project files.

---
+++ Controllers/ManagementController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Controllers
+{
+    public class ManagementController : Controller
+    {
+        private readonly DataContext categoryServices;
+        public ManagementController(DataContext CategoryServices)
+        {
+            this.categoryServices = CategoryServices;
+          
+        }
+        public IActionResult Index()
+        {
+
+            var cats = categoryServices.ImageUploads.ToList();
+            return View(cats);
+        }
+    }
+}
